7e05fbe41058073f7475ea9074afc6ad7780a816
Rishi Anand <rishi.rn818@gmail.com>
2022-04-22 23:14:13 +0530

    introduction and social icon implemented

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {HiArrowNarrowRight} from "react-icons/hi"
+
+export const Home=()=>{
+
+    return(
+        <div name="home" className="container common-width">
+            {/* container */}
+            <div>
+                <p className="greeting-para">Hi, my name is</p>
+                <h1 className="my-name">Rishi Anand</h1>
+                <h2 className="profession">I'm a Full Stack Developer</h2>
+                <p className="intro">I'm a full-stack developer specializing in building(and occassionally designing) exceptional digital experience. Currently, I'm focused on building responsive full-stack web applications.</p>
+                <div>
+                    <button className="view-work-btn">View Work<HiArrowNarrowRight className="arrow"/></button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.png'
-import {FaBars, FaTimes} from "react-icons/fa"
+import {FaBars, FaTimes,FaGithub,FaLinkedin,FaFacebook,FaTwitter,} from "react-icons/fa"
+import {HiOutlineMail} from "react-icons/hi"
+import {BsFillPersonLinesFill} from "react-icons/bs"
+
 
 export const Navbar=()=>{
-    const [toggle, setToggle]=useState(false)
+    const [toggle, setToggle]=useState(true)
     const handleToggle=()=>{
         console.log(toggle)
        return setToggle(!toggle)
@@ -33,6 +36,26 @@ export const Navbar=()=>{
                <li>Home</li>
                <li>Home</li>
            </ul>
+           {/* social icon */}
+           <div className='social-icon'>
+               <ul>
+                   <li>     
+                    <a href="/">Linkedin<FaLinkedin size={30}/></a>
+                   </li>
+                   <li>       
+                    <a href="/">Twitter<FaTwitter size={30}/></a>
+                   </li>
+                   <li>       
+                    <a href="/">GitHub<FaGithub className='github' size={30} /></a>
+                   </li>
+                   <li>       
+                    <a href="/">Email<HiOutlineMail size={30}/></a>
+                   </li>
+                   <li>       
+                    <a href="/">Resume<BsFillPersonLinesFill size={30}/></a>
+                   </li>
+               </ul>
+           </div>
        </div> 
     )
 }
